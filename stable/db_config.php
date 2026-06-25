a0fd54f411c9a53ee9a60b7bdcdd6135c760fe57
Toinou1900 <102371301+toinou1900@users.noreply.github.com>
2026-06-25 13:36:40 +0200

    Update db_config.php

--- stable/db_config.php
+++ stable/db_config.php
@@ -6,7 +6,7 @@
  */
 
 // Chemin de la base de données
-$db_path = __DIR__ . '/../dbKetamine.sqlite';
+$db_path = __DIR__ . '/../../DB_Storage/dbKetamine.sqlite';
 
 try {
     // Créer ou ouvrir la base de données SQLite

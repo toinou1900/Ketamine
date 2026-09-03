f435368090eb601496c804aa7b479ca6c361eb91
Toinou1900 <102371301+toinou1900@users.noreply.github.com>
2026-09-03 16:52:24 +0200

    .

--- stable/db_config.php
+++ stable/db_config.php
@@ -6,7 +6,7 @@
  */
 
 // Chemin de la base de données
-$db_path = __DIR__ . '/../BES-Hub/ketamine.db';
+$db_path = __DIR__ . '/../../BES-DB/dbKetamine.sqlite';
 
 try {
     // Créer ou ouvrir la base de données SQLite
